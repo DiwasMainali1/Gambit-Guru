fc82b00546cd42bc273be861c3987e3fdc2bee8a
Diwas Mainali <diwas.sok@gmail.com>
2024-05-11 12:36:25 +1000

    Made files and ranks spaced out in 8x8 grid

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -8,7 +8,7 @@ const Board = () => {
         <div className='tiles'>
             {ranks.map((rank,i) =>
                 files.map((file,j) =>
-                    <div>{rank}{file}</div>
+                    <div key={file+'-'+rank}>{file}{rank}</div>
                 )
             )}
         </div>
