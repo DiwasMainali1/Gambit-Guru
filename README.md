255dd081a815394119cf7205ffcf30ce1786e07a
Diwas Mainali <diwas.sok@gmail.com>
2024-05-30 14:16:38 +1000

    Major changes, made everything in one html file

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ Welcome to Gambit Guru, a powerful and intuitive chess opening trainer web app b
 
 ## Features (Upon Completion)
 
-- **Extensive Opening Database**: Gambit Guru boasts a comprehensive database of chess openings and their variations, ensuring you have access to a wide range of strategies to study and practice.
+-   **Extensive Opening Database**: Gambit Guru boasts a comprehensive database of chess openings and their variations, ensuring you have access to a wide range of strategies to study and practice.
 
-- **Interactive Training**: Engage in interactive training sessions where you can practice playing through your chosen openings and their variations. Gambit Guru provides a user-friendly interface that allows you to make moves and receive immediate feedback on your choices.
+-   **Interactive Training**: Engage in interactive training sessions where you can practice playing through your chosen openings and their variations. Gambit Guru provides a user-friendly interface that allows you to make moves and receive immediate feedback on your choices.
 
-- **Customizable Practice**: Tailor your training experience to your preferences. Choose specific openings to focus on, set the difficulty level, and adjust the time controls to match your learning pace.
+-   **Customizable Practice**: Tailor your training experience to your preferences. Choose specific openings to focus on, set the difficulty level, and adjust the time controls to match your learning pace.
 
-- **Opening Explorer**: Dive deep into the world of chess openings with Gambit Guru's opening explorer. Browse through various openings, view their key positions, and learn about their strategic ideas and common traps.
+-   **Opening Explorer**: Dive deep into the world of chess openings with Gambit Guru's opening explorer. Browse through various openings, view their key positions, and learn about their strategic ideas and common traps.
 
-- **Progress Tracking**: Monitor your progress and identify areas for improvement. Gambit Guru keeps track of your training history, providing statistics and insights to help you analyze your strengths and weaknesses.
+-   **Progress Tracking**: Monitor your progress and identify areas for improvement. Gambit Guru keeps track of your training history, providing statistics and insights to help you analyze your strengths and weaknesses.
 
-- **Mobile-Friendly**: Access Gambit Guru on the go with its mobile-friendly design. Whether you're using a desktop, tablet, or smartphone, you can enjoy a seamless training experience across devices.
+-   **Mobile-Friendly**: Access Gambit Guru on the go with its mobile-friendly design. Whether you're using a desktop, tablet, or smartphone, you can enjoy a seamless training experience across devices.
 
 ## Feedback and Support
 
--- main.js
+++ main.js
@@ -1,56 +1,102 @@
-const changeButton = document.getElementById("switchButton");
+const changeButton = document.getElementById("switchButton"); 
 changeButton.addEventListener("click", changeBoard);
+
 const resetButton = document.getElementById('resetButton');
 resetButton.addEventListener('click', resetBoard);
-import { whiteChessUtilities } from "./helper.js";
-import { blackChessUtilities } from "./helper.js";
-let oMoves = [
-    ["White-Pawn", "c3"],
-    ["White-King", "g1"],
-    ["White-Bishop", "b3"],
-    ["White-Pawn", "d3"],
-    ["White-Bishop", "a4"],
-    ["White-Bishop", "b5"],
-    ["White-Knight", "f3"],
-    ["White-Pawn", "e4"],
-];
-let bMoves = [
-    ["d7", "d6"],
-    ["f8", "c5"],
-    ["b7", "b5"],
-    ["g8", "f6"],
-    ["a7", "a6"],
-    ["b8", "c6"],
-    ["e7", "e5"],
-];
-whiteChessUtilities.createBoard(0, oMoves, bMoves);
 
+const ruyLopezButton = document.getElementById('ruyLopez');
+const sicilianDefenseButton = document.getElementById('sicilianDefense');
+const frenchDefenseButton = document.getElementById('frenchDefense');
+const queensGambitButton = document.getElementById('queensGambit');
+const kingsIndianDefenseButton = document.getElementById('kingsIndianDefense');
+const caroKannDefenseButton = document.getElementById('caroKannDefense');
+
+import { whiteChessUtilities, blackChessUtilities } from "./helpers/helper.js";
+
+whiteChessUtilities.createBoard(0, [], []);
 whiteChessUtilities.addEventListeners();
 
-function changeBoard() {
-  let board = document.querySelector("#Board");
-  let pieceColour = board.childNodes[0].firstChild.id;
-  if (pieceColour.includes("Black")) {
-    board.innerHTML = "";
-    blackChessUtilities.createBoard();
-    blackChessUtilities.addEventListeners();
-  } else {
-    board.innerHTML = "";
-    whiteChessUtilities.createBoard();
+ruyLopezButton.addEventListener('click', () => {
+    let board = document.querySelector("#Board");
+    let pieceColour = board.childNodes[0].firstChild.id;
+    
+    if (pieceColour.includes("White")) {
+        changeBoard();
+    }
+    board.innerHTML = '';
+    let ruyLopezMoves = [
+        ["White-Pawn", "c3"],
+        ["White-King", "g1"],
+        ["White-Bishop", "b3"],
+        ["White-Pawn", "d3"],
+        ["White-Bishop", "a4"],
+        ["White-Bishop", "b5"],
+        ["White-Knight", "f3"],
+        ["White-Pawn", "e4"],
+    ];   
+    let blackMoves = [
+        ["d7", "d6"],
+        ["f8", "c5"],
+        ["b7", "b5"],
+        ["g8", "f6"],
+        ["a7", "a6"],
+        ["b8", "c6"],
+        ["e7", "e5"],
+    ];
+    whiteChessUtilities.createBoard(1, ruyLopezMoves, blackMoves);
     whiteChessUtilities.addEventListeners();
-  }
-}
+});
 
-export function resetBoard() {
+sicilianDefenseButton.addEventListener('click', () => {
     let board = document.querySelector("#Board");
     let pieceColour = board.childNodes[0].firstChild.id;
     if (pieceColour.includes("Black")) {
-      board.innerHTML = "";
-      whiteChessUtilities.createBoard(0, oMoves, bMoves);
-      whiteChessUtilities.addEventListeners();
+        changeBoard();
+    }
+    board.innerHTML = '';
+    let sicilianMoves = [
+        ["Black-Pawn", "b3"],
+        ["Black-Pawn", "e3"],
+        ["Black-Pawn", "e5"],
+        ["Black-Knight", "f3"],
+        ["Black-Pawn", "f4"],
+    ];
+    let whiteMoves = [
+        ["g8", "f6"],
+        ["c6", "e5"],
+        ["e7", "e5"],
+        ["b8", "c6"],
+    ];   
+    blackChessUtilities.createBoard(1, sicilianMoves, whiteMoves);
+    blackChessUtilities.addEventListeners();
+});
+
+function changeBoard() {
+   let board = document.querySelector("#Board");
+   let pieceColour = board.childNodes[0].firstChild.id;
+   
+    if (pieceColour.includes("Black")) {
+       board.innerHTML = "";
+       blackChessUtilities.createBoard(0, [], []);
+       blackChessUtilities.addEventListeners();
     } else {
-      board.innerHTML = "";
-      blackChessUtilities.createBoard();
-      blackChessUtilities.addEventListeners();
+       board.innerHTML = "";
+       whiteChessUtilities.createBoard(0, [], []);
+       whiteChessUtilities.addEventListeners();
+    }
+}
+
+function resetBoard() {
+   let board = document.querySelector("#Board");
+   let pieceColour = board.childNodes[0].firstChild.id;
+   
+    if (pieceColour.includes("Black")) {
+       board.innerHTML = "";
+       whiteChessUtilities.createBoard(0, [], []);
+       whiteChessUtilities.addEventListeners();
+    } else {
+       board.innerHTML = "";
+       blackChessUtilities.createBoard(0, [], []);
+       blackChessUtilities.addEventListeners();
     }
 }
