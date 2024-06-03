db8538e7a31d7f05e4ad0b0a661ba382fe785d4f
Diwas Mainali <diwas.sok@gmail.com>
2024-06-04 09:48:43 +1000

    Fixed style, fixed black pieces going out of the board for mobile devices

--- helpers/helper.js
+++ helpers/helper.js
@@ -896,7 +896,7 @@ const blackChessUtilities = (function() {
 		});
 	}
 
-	function dragStart(e) {
+    function dragStart(e) {
         if (e.type === "touchstart") {
             draggedElement = e.target.tagName.toLowerCase() === "img" ? e.target : null;
             if (draggedElement) {
@@ -909,9 +909,14 @@ const blackChessUtilities = (function() {
                 ghostImage.style.height = draggedElement.offsetHeight + "px";
                 
                 document.body.appendChild(ghostImage);
+                
                 const touch = e.touches[0];
-                ghostImage.style.left = touch.clientX - ghostImage.offsetWidth / 2 + "px";
-                ghostImage.style.top = touch.clientY - ghostImage.offsetHeight / 2 + "px";
+                const rect = draggedElement.getBoundingClientRect();
+                const offsetX = touch.clientX - rect.left;
+                const offsetY = touch.clientY - rect.top;
+                
+                ghostImage.style.left = touch.clientX - offsetX + "px";
+                ghostImage.style.top = touch.clientY - offsetY + "px";
                 e.preventDefault();
             }
         } else {
@@ -921,34 +926,52 @@ const blackChessUtilities = (function() {
             else e.preventDefault();
             draggedSquare = e.target.parentNode;
         }
-	}
+    }
 
-	function dragOver(e) {
+    function dragOver(e) {
         if (e.type === "touchmove") {
             const ghostImage = document.querySelector(".ghost-image");
             if (ghostImage) {
                 const touch = e.touches[0];
-                ghostImage.style.left = touch.clientX - ghostImage.offsetWidth / 2 + "px";
-                ghostImage.style.top = touch.clientY - ghostImage.offsetHeight / 2 + "px";
+                const boardRect = board.getBoundingClientRect();
+    
+                if (
+                    touch.clientX < boardRect.left ||
+                    touch.clientX > boardRect.right ||
+                    touch.clientY < boardRect.top ||
+                    touch.clientY > boardRect.bottom
+                ) {
+                    // If the touch position is outside the board, remove the ghost image
+                    ghostImage.remove();
+                    draggedElement.style.visibility = "visible";
+                    draggedSquare.appendChild(draggedElement);
+                    removeColour(specialSquares);
+                    return;
+                } else {
+                    // If the touch position is within the board, update the ghost image position
+                    ghostImage.style.left = touch.clientX - ghostImage.offsetWidth / 2 + "px";
+                    ghostImage.style.top = touch.clientY - ghostImage.offsetHeight / 2 + "px";
+                }
             }
             e.preventDefault();
-        }
-		else {
+        } else {
             e.dataTransfer.dropEffect = "move";
             e.preventDefault();
         }
-	}
+    }
 
 	function dragDrop(e) {
         const ghostImage = document.querySelector(".ghost-image");
         let pieceNode, hasCapture, squareId, hasSpan; 
         if (e.type === "touchend") {
-            ghostImage.remove();
             let touch = e.changedTouches[0];
             pieceNode = document.elementFromPoint(touch.clientX, touch.clientY);
             hasSpan = pieceNode.querySelector("span") !== null;
-            hasCapture = pieceNode.parentNode.style.backgroundColor === "rgb(100, 110, 64)";
+            hasCapture = pieceNode.parentNode && pieceNode.parentNode.style.backgroundColor === "rgb(100, 110, 64)";
             squareId = pieceNode.getAttribute("square-id");
+            if (ghostImage) {
+                ghostImage.remove();
+            }        
         } else {
             pieceNode = e.target;
             hasSpan = pieceNode.querySelector("span") !== null;
